a9b9df8dbd253e260254ab9392f04977af897ca6
ji-web <81374830+ji-web@users.noreply.github.com>
2021-09-29 14:49:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/WEKXetAxL/model.json",modelReady);
+
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
